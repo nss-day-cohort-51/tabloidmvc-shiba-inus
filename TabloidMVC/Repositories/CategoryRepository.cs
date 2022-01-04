16f60b90a15f107a794a09d3efb9ce8279e80e18
Chris Murphy <theworldisyours@gmail.com>
2022-01-04 14:44:12 -0500

    Grant user the ability to Delete a Category

--- TabloidMVC/Repositories/CategoryRepository.cs
+++ TabloidMVC/Repositories/CategoryRepository.cs
@@ -116,6 +116,21 @@ namespace TabloidMVC.Repositories
         }
 
 
+        public void Delete(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"DELETE FROM Category WHERE id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+
 
     }
 }
