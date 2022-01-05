276c169a886cee7f3853395858fb1d9f54f23e12
Colin Miller <colinraymiller@gmail.com>
2022-01-04 18:53:56 -0600

    small change:

--- TabloidMVC/Controllers/PostController.cs
+++ TabloidMVC/Controllers/PostController.cs
@@ -67,157 +67,158 @@ namespace TabloidMVC.Controllers
             {
                 return View(comment);
             }
+        }
 
-            public IActionResult Index()
-            {
-                var posts = _postRepository.GetAllPublishedPosts();
-                return View(posts);
-            }
+        public IActionResult Index()
+        {
+            var posts = _postRepository.GetAllPublishedPosts();
+            return View(posts);
+        }
 
-            public IActionResult IndexMyPosts()
+        public IActionResult IndexMyPosts()
+        {
+            int userId = GetCurrentUserProfileId();
+
+            var posts = _postRepository.GetPostsByUserId(userId);
+            return View(posts);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var post = _postRepository.GetPublishedPostById(id);
+            if (post == null)
             {
                 int userId = GetCurrentUserProfileId();
-
-                var posts = _postRepository.GetPostsByUserId(userId);
-                return View(posts);
-            }
-
-            public IActionResult Details(int id)
-            {
-                var post = _postRepository.GetPublishedPostById(id);
-                if (post == null)
-                {
-                    int userId = GetCurrentUserProfileId();
-                    post = _postRepository.GetUserPostById(id, userId);
-                    if (post == null)
-                    {
-                        return NotFound();
-                    }
-                }
-                return View(post);
-            }
-
-            public IActionResult Create()
-            {
-                var vm = new PostCreateViewModel();
-                vm.CategoryOptions = _categoryRepository.GetAllCategories();
-                return View(vm);
-            }
-
-            [HttpPost]
-            public IActionResult Create(PostCreateViewModel vm)
-            {
-                try
-                {
-                    vm.Post.CreateDateTime = DateAndTime.Now;
-                    vm.Post.IsApproved = true;
-                    vm.Post.UserProfileId = GetCurrentUserProfileId();
-
-                    _postRepository.Add(vm.Post);
-
-                    return RedirectToAction("Details", new { id = vm.Post.Id });
-                }
-                catch
-                {
-                    vm.CategoryOptions = _categoryRepository.GetAllCategories();
-                    return View(vm);
-                }
-            }
-
-            [Authorize]
-            public ActionResult Edit(int id)
-            {
-                var post = _postRepository.GetPublishedPostById(id);
-
+                post = _postRepository.GetUserPostById(id, userId);
                 if (post == null)
                 {
                     return NotFound();
                 }
-
-                return View(post);
             }
+            return View(post);
+        }
 
-            // POST: PostController/Edit/5
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public ActionResult Edit(int id, Post post)
+        public IActionResult Create()
+        {
+            var vm = new PostCreateViewModel();
+            vm.CategoryOptions = _categoryRepository.GetAllCategories();
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Create(PostCreateViewModel vm)
+        {
+            try
             {
-                try
-                {
+                vm.Post.CreateDateTime = DateAndTime.Now;
+                vm.Post.IsApproved = true;
+                vm.Post.UserProfileId = GetCurrentUserProfileId();
 
-                    _postRepository.Update(post);
+                _postRepository.Add(vm.Post);
 
-                    return RedirectToAction("Index");
-                }
-                catch (Exception ex)
-                {
-                    return View(post);
-                }
+                return RedirectToAction("Details", new { id = vm.Post.Id });
             }
-
-            [Authorize]
-            public ActionResult Delete(int id)
+            catch
             {
-                Post post = _postRepository.GetPublishedPostById(id);
-
-                return View(post);
-            }
-
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-
-            public ActionResult Delete(int id, Post post)
-            {
-                try
-                {
-                    _postRepository.Delete(id);
-
-                    return RedirectToAction(nameof(Index));
-                }
-                catch (Exception ex)
-                {
-                    return View(post);
-                }
-            }
-
-            private int GetCurrentUserProfileId()
-            {
-                string id = User.FindFirstValue(ClaimTypes.NameIdentifier);
-                return int.Parse(id);
-            }
-
-            public IActionResult ManageTags(int id)
-            {
-                List<PostTag> postTags = _postTagRepository.GetPostTagsByPostId(id);
-                return View(postTags);
-            }
-
-            public IActionResult AddPostTag(int id)
-            {
-                var vm = new AddPostTagViewModel();
-                vm.PostId = id;
-                vm.Tags = _tagRepository.GetTagsAvailableForPost(id);
-                vm.PostTag = new PostTag()
-                {
-                    PostId = id
-                };
-
+                vm.CategoryOptions = _categoryRepository.GetAllCategories();
                 return View(vm);
             }
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public IActionResult AddPostTag(int id, AddPostTagViewModel vm)
+        }
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var post = _postRepository.GetPublishedPostById(id);
+
+            if (post == null)
             {
-                try
-                {
-                    _tagRepository.AddPostTag(vm.PostTag);
-                    return RedirectToAction(nameof(ManageTags), new { id = vm.PostTag.PostId });
-                }
-                catch (Exception ex)
-                {
-                    return RedirectToAction(nameof(ManageTags), new { id = vm.PostTag.PostId });
-                }
+                return NotFound();
             }
 
+            return View(post);
         }
+
+        // POST: PostController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Post post)
+        {
+            try
+            {
+
+                _postRepository.Update(post);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View(post);
+            }
+        }
+
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            Post post = _postRepository.GetPublishedPostById(id);
+
+            return View(post);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Delete(int id, Post post)
+        {
+            try
+            {
+                _postRepository.Delete(id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return View(post);
+            }
+        }
+
+        private int GetCurrentUserProfileId()
+        {
+            string id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return int.Parse(id);
+        }
+
+        public IActionResult ManageTags(int id)
+        {
+            List<PostTag> postTags = _postTagRepository.GetPostTagsByPostId(id);
+            return View(postTags);
+        }
+
+        public IActionResult AddPostTag(int id)
+        {
+            var vm = new AddPostTagViewModel();
+            vm.PostId = id;
+            vm.Tags = _tagRepository.GetTagsAvailableForPost(id);
+            vm.PostTag = new PostTag()
+            {
+                PostId = id
+            };
+
+            return View(vm);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddPostTag(int id, AddPostTagViewModel vm)
+        {
+            try
+            {
+                _tagRepository.AddPostTag(vm.PostTag);
+                return RedirectToAction(nameof(ManageTags), new { id = vm.PostTag.PostId });
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction(nameof(ManageTags), new { id = vm.PostTag.PostId });
+            }
+        }
+
     }
+}
