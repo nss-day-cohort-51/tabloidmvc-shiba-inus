2a242f04c9b11a26c4a82ab42b8c604da2af781a
Colin Miller <colinraymiller@gmail.com>
2022-01-05 15:06:36 -0600

    Only Admins Can deactivate accounts

---
+++ TabloidMVC/Models/ViewModels/UserProfileIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TabloidMVC.Models.ViewModels
+{
+    public class UserProfileIndexViewModel
+    {
+        public List<UserProfile> UserProfiles { get; set; }
+        public bool IsAdmin { get; set; }
+
+    }
+}
