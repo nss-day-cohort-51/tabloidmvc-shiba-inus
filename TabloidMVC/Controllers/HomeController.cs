824d199e133215d556bb4e8daa48697507238491
Colin Miller <colinraymiller@gmail.com>
2022-01-07 09:36:35 -0600

    fixed homepage not loading

--- TabloidMVC/Controllers/HomeController.cs
+++ TabloidMVC/Controllers/HomeController.cs
@@ -26,6 +26,13 @@ namespace TabloidMVC.Controllers
 
         public IActionResult Index()
         {
+            if (User.FindFirstValue(ClaimTypes.Email) != null)
+            {
+                int currentUserId = _userProfileRepository.GetByEmail(User.FindFirstValue(ClaimTypes.Email)).Id;
+                List<Post> subscribedPosts = _postRepository.GetAllSubscribedPosts(currentUserId);
+                return View(subscribedPosts);
+            }
+
             return View();
         }
 
